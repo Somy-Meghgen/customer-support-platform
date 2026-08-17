73824cd51de258129cd6c4a871a6d53a44108a8b
Somy-Meghgen <somykumar.giri@meghgen.com>
2026-08-17 13:34:27 +0530

    Revert "CSP-3 Improve Customer API Logging"

--- src/main/java/LoggerUtil.java
+++ src/main/java/LoggerUtil.java
@@ -2,13 +2,7 @@ public class LoggerUtil {
 
     public static void log(String message){
 
-        System.out.println("[INFO] " + message);
-
-    }
-
-    public static void error(String message){
-
-        System.out.println("[ERROR] " + message);
+        System.out.println(message);
 
     }
 
